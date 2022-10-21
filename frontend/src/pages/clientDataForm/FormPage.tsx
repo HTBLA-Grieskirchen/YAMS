fa990c45c2bcf9471d3673224e02beeda7d79573
domiiii1320 <wernsdorferd180125@sus.htl-grieskirchen.at>
2022-10-21 10:43:43 +0200

    Implement form to add client to database

---
+++ frontend/src/pages/clientDataForm/FormPage.tsx
@@ -0,0 +1,12 @@
+import Form from "../../components/clientDataForm/Form"
+import {NextPage} from "next";
+
+const FormPage: NextPage = () => {
+    return (
+        <>
+            <Form/>
+        </>
+    )
+}
+
+export default FormPage

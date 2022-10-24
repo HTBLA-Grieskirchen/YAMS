a707f3c48de2a076e55a12d96fea65eeea388403
TimerErTim <timerertim@gmail.com>
2022-10-24 13:09:55 +0200

    Add Notification TODO

--- frontend/src/libs/notification.ts
+++ frontend/src/libs/notification.ts
@@ -51,6 +51,7 @@ export type NotificationContent = {
 
 // TODO: Add possibility to also display notification on host system if in Tauri
 // TODO: Add support for undefined duration which keeps the notification open till manuel closure
+// TODO: Add support for async actions
 const notification = {
     /**
      * This opens an info notification in the lower right corner of the screen. The content and actions can be specified

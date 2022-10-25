f5bab266a363ae1bd1b1cf9a005956aa255dbaf8
TimerErTim <timerertim@gmail.com>
2022-10-25 11:01:03 +0200

    Improve Notification Scroll Position

--- frontend/src/components/Notifications.tsx
+++ frontend/src/components/Notifications.tsx
@@ -16,7 +16,7 @@ const Notifications = observer(() => {
     }, [notifications.current?.scrollHeight])
 
     return <div ref={notifications}
-                className="fixed bottom-4 right-4 space-y-2 pl-16 pt-8 max-w-full max-h-full overflow-y-auto overscroll-auto hover:overscroll-contain">
+                className="fixed bottom-0 right-2 space-y-2 pl-16 pt-8 pb-4 pr-2 max-w-full max-h-full overflow-y-auto overscroll-auto hover:overscroll-contain">
         {store.notificationStore.currentNavigations().map((notification, index) =>
             <NotificationComponent key={index} notification={notification}/>)}
     </div>

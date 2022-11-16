c2e856899ba506fbe78267c4469b4bc70e3f5245
david <RathmairD180116@sus.htl-grieskirchen.at>
2022-11-16 12:31:15 +0100

    Added ClientRegisterEditingForm.tsx in ../components

---
+++ frontend/src/components/ClientRegisterEditingForm.tsx
@@ -0,0 +1,19 @@
+
+
+const EditClientForm: any = () => {
+
+
+    return (
+        <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+            <div>
+
+
+
+            </div>
+        </div>
+    )
+
+
+
+}
+export default EditClientForm

75ff29b8f71b14073bf803124f1f1cb7c393bdc7
domiiii1320 <wernsdorferd180125@sus.htl-grieskirchen.at>
2022-11-17 11:25:40 +0100

    Add necessary states and a handler-method for deleting a client

--- frontend/src/components/ClientItem.tsx
+++ frontend/src/components/ClientItem.tsx
@@ -1,8 +1,24 @@
 import {observer} from "mobx-react";
 import Client from "../model/client"
 import {LiveRefresher} from "../libs/database";
+import {useState} from "react";
 
 const ClientItem = observer(({client, refresh}: { client: Client, refresh: LiveRefresher }) => {
+    const [lastName, setLastName] = useState(client.lastName)
+    const [firstName, setFirstName] = useState(client.firstName)
+    const [birthDate, setBirthDate] = useState(client.birthdate)
+    const [email, setEmail] = useState(client.email)
+    const [consent, setConsent] = useState(client.consent)
+    const [telNumber, setTelNumber] = useState(client.mobileNumber)
+    //TODO: Add address information
+
+    const [deleteSubmitted, setDeleteSubmitted] = useState(false)
+
+    const deleteSubmit = async () => {
+        setDeleteSubmitted(true)
+        //TODO: finish implementing
+    }
+
     return (
         <div>
 

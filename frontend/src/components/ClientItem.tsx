b8c3552fd0bca93aded8980765f9c580b9f88b05
domiiii1320 <wernsdorferd180125@sus.htl-grieskirchen.at>
2022-11-18 11:46:02 +0100

    Delete unused states

--- frontend/src/components/ClientItem.tsx
+++ frontend/src/components/ClientItem.tsx
@@ -4,14 +4,6 @@ import {LiveRefresher} from "../libs/database";
 import {useState} from "react";
 
 const ClientItem = observer(({client, refresh}: { client: Client, refresh: LiveRefresher }) => {
-    const [lastName, setLastName] = useState(client.lastName)
-    const [firstName, setFirstName] = useState(client.firstName)
-    const [birthDate, setBirthDate] = useState(client.birthdate)
-    const [email, setEmail] = useState(client.email)
-    const [consent, setConsent] = useState(client.consent)
-    const [telNumber, setTelNumber] = useState(client.mobileNumber)
-    //TODO: Add address information
-
     const [deleteSubmitted, setDeleteSubmitted] = useState(false)
 
     const deleteSubmit = async () => {

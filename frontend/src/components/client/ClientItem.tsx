d68d0bf3b06d0a4ed07c7f22eaa9cdb311c7d380
domiiii1320 <wernsdorferd180125@sus.htl-grieskirchen.at>
2022-11-18 12:13:52 +0100

    Add default export

--- frontend/src/components/client/ClientItem.tsx
+++ frontend/src/components/client/ClientItem.tsx
@@ -1,27 +1,20 @@
 import {observer} from "mobx-react";
-import Client from "../../model/client"
-import {LiveRefresher, query} from "../../libs/database";
-import React, {useState} from "react";
-import {Result} from "surrealdb.js";
-import AnimalList from "../animal/AnimalList";
+import Client from "../model/client"
+import {LiveRefresher} from "../libs/database";
+import {useState} from "react";
 
-const ClientItem = observer(({client, refresher}: { client: Client, refresher: LiveRefresher }) => {
+const ClientItem = observer(({client, refresh}: { client: Client, refresh: LiveRefresher }) => {
     const [deleteSubmitted, setDeleteSubmitted] = useState(false)
-    const [showDetail, setShowDetail] = useState(false)
 
     const deleteSubmit = async () => {
         setDeleteSubmitted(true)
-
-        const result = await deleteClient(client)
-
-        await refresher()
-        setDeleteSubmitted(false)
+        //TODO: finish implementing
     }
 
     return (
-        <div className="flex flex-col w-fit">
-            <div className="flex flex-row space-x-4 items-center">
-                <div className="flex flex-row space-x-2">
+        <div className="flex flex-col">
+            <div className="flex flex-row space-x-6 items-center">
+                <div className="flex flex-row space-x-4">
                     <div className="flex flex-col">
                         <label className="text-gray-700 text-sm sm:w-48 w-fit">
                             Last Name
@@ -43,23 +36,41 @@ const ClientItem = observer(({client, refresher}: { client: Client, refresher: L
                             Birth Date
                         </label>
                         <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                            {client.birthdate.toLocaleDateString()}
+                            {client.birthdate.toDateString()}
                         </p>
                     </div>
-
+                    <div className="flex flex-col">
+                        <label className="text-gray-700 text-sm sm:w-48 w-fit">
+                            E-Mail
+                        </label>
+                        <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
+                            {client.email}
+                        </p>
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-gray-700 text-sm sm:w-48 w-fit">
+                            E-Mail
+                        </label>
+                        {client.consent ?
+                            <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
+                                Present
+                            </p>
+                            :
+                            <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
+                                Not Present
+                            </p>
+                        }
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-gray-700 text-sm sm:w-48 w-fit">
+                            Mobile Number
+                        </label>
+                        <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
+                            {client.mobile_number}
+                        </p>
+                    </div>
                 </div>
                 <div className="flex flex-row h-full items-center space-x-1">
-                    <button
-                        type="button"
-                        onClick={() => setShowDetail(!showDetail)}
-                        className="align-text-bottom text-2xl hover:text-3xl hover:text-gray-600 text-gray-400 w-8 h-8 transition-all">
-                        {showDetail ?
-                            <i className="fa-solid fa-caret-up"/>
-                            :
-                            <i className="fa-solid fa-caret-down"/>
-                        }
-
-                    </button>
                     <button type="button" onClick={e => {
                         deleteSubmit()
                     }} disabled={deleteSubmitted}
@@ -68,51 +79,8 @@ const ClientItem = observer(({client, refresher}: { client: Client, refresher: L
                     </button>
                 </div>
             </div>
-            {showDetail ?
-                <div className="bg-white rounded-xl p-2 w-fit mt-2">
-                    <div className="flex flex-col">
-                        <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                            {client.email}
-                        </p>
-                    </div>
-                    <div className="flex flex-col">
-                        {client.consent ?
-                            <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                                Consent given
-                            </p>
-                            :
-                            <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                                Consent not given
-                            </p>
-                        }
-                    </div>
-                    <div className="flex flex-col">
-                        <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                            {client.mobile_number}
-                        </p>
-                    </div>
-                    {deleteSubmitted ?
-                        <></>
-                        :
-                        <AnimalList client={client}/>
-                    }
-                </div>
-                :
-                <div></div>
-            }
         </div>
     )
 })
-
-export async function deleteClient(client: Client): Promise<Result<any>> {
-    const response = await query("DELETE type::thing($landTable, $landID)", {
-        landTable: Client.TABLE_NAME,
-        landID: client.id
-    })
-
-    return response[0] ?? {
-        error: new Error("No response at all")
-    }
-}
 
 export default ClientItem

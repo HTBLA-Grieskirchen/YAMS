bd67ef52dd2ed343b96edf7fca0cf75e1d1bfac0
domiiii1320 <wernsdorferd180125@sus.htl-grieskirchen.at>
2022-11-18 12:49:43 +0100

    Add button in order to add new clients

--- frontend/src/pages/client/overview/index.tsx
+++ frontend/src/pages/client/overview/index.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import {useLive} from "../../../libs/database";
 import Client from "../../../model/client";
 import ClientItem from "../../../components/ClientItem";
+import {useState} from "react";
+import ClientRegisterForm from "../../../components/ClientRegisterForm";
 
 const ClientOverview = observer(() => {
     const [clientsRaw, refreshClients] = useLive("SELECT * FROM CLIENTS ORDER BY last_name")
@@ -13,6 +15,8 @@ const ClientOverview = observer(() => {
         }
     }).filter((it: any) => it !== undefined) : []
 
+    const [addEntry, setAddEntry] = useState(false)
+
     return (
         <>
             <Head>
@@ -20,6 +24,20 @@ const ClientOverview = observer(() => {
             </Head>
 
             <main className="flex flex-col w-fill m-5 p-3 rounded-lg bg-gray-200 shadow">
+                <div className="flex">
+                    {addEntry ?
+                        <div>
+                            <ClientRegisterForm/>
+                        </div>
+                        :
+                        <div>
+                            <button onClick={e => setAddEntry(true)}
+                                    className="m-1 w-24 h-10 hover:text-lg border border-black rounded shadow bg-blue-100 hover:bg-blue-300 transition-all">
+                                New Client
+                            </button>
+                        </div>
+                    }
+                </div>
                 <div className="flex flex-col pt-3">
                     {clients.length > 0 ?
                         <div className="divide-gray-400 divide-y">

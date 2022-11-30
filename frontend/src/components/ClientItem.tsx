61c231f24b07ec7b686e6f8bde6092ea06836b10
domiiii1320 <wernsdorferd180125@sus.htl-grieskirchen.at>
2022-11-30 09:48:27 +0100

    Change ClientItem to show details when the item is clicked

--- frontend/src/components/ClientItem.tsx
+++ frontend/src/components/ClientItem.tsx
@@ -5,6 +5,7 @@ import {useState} from "react";
 
 const ClientItem = observer(({client, refresh}: { client: Client, refresh: LiveRefresher }) => {
     const [deleteSubmitted, setDeleteSubmitted] = useState(false)
+    const [showDetail, setShowDetail] = useState(false)
 
     const deleteSubmit = async () => {
         setDeleteSubmitted(true)
@@ -12,9 +13,9 @@ const ClientItem = observer(({client, refresh}: { client: Client, refresh: LiveR
     }
 
     return (
-        <div className="flex flex-col">
-            <div className="flex flex-row space-x-6 items-center">
-                <div className="flex flex-row space-x-4">
+        <div className="flex flex-col w-fit" onClick={() => setShowDetail(!showDetail)}>
+            <div className="flex flex-row space-x-4 items-center">
+                <div className="flex flex-row space-x-2">
                     <div className="flex flex-col">
                         <label className="text-gray-700 text-sm sm:w-48 w-fit">
                             Last Name
@@ -36,37 +37,7 @@ const ClientItem = observer(({client, refresh}: { client: Client, refresh: LiveR
                             Birth Date
                         </label>
                         <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                            {client.birthdate.toDateString()}
-                        </p>
-                    </div>
-                    <div className="flex flex-col">
-                        <label className="text-gray-700 text-sm sm:w-48 w-fit">
-                            E-Mail
-                        </label>
-                        <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                            {client.email}
-                        </p>
-                    </div>
-                    <div className="flex flex-col">
-                        <label className="text-gray-700 text-sm sm:w-48 w-fit">
-                            E-Mail
-                        </label>
-                        {client.consent ?
-                            <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                                Present
-                            </p>
-                            :
-                            <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                                Not Present
-                            </p>
-                        }
-                    </div>
-                    <div className="flex flex-col">
-                        <label className="text-gray-700 text-sm sm:w-48 w-fit">
-                            Mobile Number
-                        </label>
-                        <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
-                            {client.mobile_number}
+                            {client.birthdate.toLocaleDateString()}
                         </p>
                     </div>
                 </div>
@@ -79,6 +50,42 @@ const ClientItem = observer(({client, refresh}: { client: Client, refresh: LiveR
                     </button>
                 </div>
             </div>
+            {showDetail ?
+                <div className="bg-white rounded-xl p-2 w-fit mt-2">
+                    <div className="border-dashed border-2 rounded-xl border-gray-300 p-2">
+                        <label className="text-gray-700 text-sm sm:w-48 w-fit">
+                            E-Mail
+                        </label>
+                        <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
+                            {client.email}
+                        </p>
+                    </div>
+                    <div className="">
+                        <label className="text-gray-700 text-sm sm:w-48 w-fit">
+                            Consent
+                        </label>
+                        {client.consent ?
+                            <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
+                                Present
+                            </p>
+                            :
+                            <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
+                                Not Present
+                            </p>
+                        }
+                    </div>
+                    <div className="">
+                        <label className="text-gray-700 text-sm sm:w-48 w-fit">
+                            Mobile Number
+                        </label>
+                        <p className="text-lg min-w-full xl:max-w-4xl sm:max-w-sm max-w-0 truncate">
+                            {client.mobile_number}
+                        </p>
+                    </div>
+                </div>
+                :
+                <div></div>
+            }
         </div>
     )
 })

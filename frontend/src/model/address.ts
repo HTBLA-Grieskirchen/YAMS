616a99361fc658012c65c4e97c06d8d7a4a16134
TimerErTim <timerertim@gmail.com>
2022-12-11 01:05:43 +0100

    Improve database related stores
    Add typesafety to raw response
    Abstract common operations
    Reduce redundant reactivity through MobX

--- frontend/src/model/address.ts
+++ frontend/src/model/address.ts
@@ -1,6 +1,7 @@
-import {makeRecordForTable, Record, SurrealObject} from "./surreal";
+import {makeRecordForTable, Record, SurrealObject, SurrealResponse} from "./surreal";
 import City from "./city";
 import {makeAutoObservable} from "mobx";
+import store from "../stores";
 
 export default class Address implements SurrealObject {
     static readonly TABLE: string = "address"
@@ -17,5 +18,49 @@ export default class Address implements SurrealObject {
         this.extra = extra
 
         makeAutoObservable(this)
+    }
+}
+
+export class AddressResponse implements SurrealResponse<Address> {
+    public data: {
+        id: string,
+        city: string,
+        street: string,
+        extra?: string
+    }
+
+    private constructor(data: AddressResponse["data"]) {
+        this.data = data
+    }
+
+    static from(item: any): AddressResponse | undefined {
+        if (
+            item.id === undefined ||
+            item.city === undefined || item.street === undefined
+        ) return
+
+        return new AddressResponse(item)
+    }
+
+    applyOn(object: Address): void {
+        // Check if data is meant for object
+        if (object.record.join() == this.data.id) return
+
+        if (this.data.city != object.city.record.join()) {
+            let city = store.addressStore.indexedCities.get(this.data.city)
+            if (city) object.city = city
+        }
+
+        if (this.data.street != object.street) object.street = this.data.street
+
+        const extra = this.data.extra ?? ""
+        if (extra != object.extra) object.extra = extra
+    }
+
+    intoObject(): Address | undefined {
+        let city = store.addressStore.indexedCities.get(this.data.city)
+        if (!city) return
+
+        return new Address(this.data.id, city, this.data.street, this.data.extra ?? "")
     }
 }
--- frontend/src/model/country.ts
+++ frontend/src/model/country.ts
@@ -1,5 +1,5 @@
 import {makeAutoObservable} from "mobx";
-import {makeRecordForTable, Record, SurrealObject} from "./surreal";
+import {makeRecordForTable, Record, SurrealObject, SurrealResponse} from "./surreal";
 
 export default class Country implements SurrealObject {
     static readonly TABLE: string = "country"
@@ -14,5 +14,47 @@ export default class Country implements SurrealObject {
         this.short = short
 
         makeAutoObservable(this)
+    }
+
+    static assertDatabaseFields(item: any) {
+        return item.id !== undefined && item.name !== undefined && item.short !== undefined
+    }
+
+    updateDatabaseFields(item: any) {
+        this.name = item.name
+        this.short = item.short
+    }
+}
+
+export class CountryResponse implements SurrealResponse<Country> {
+    public data: {
+        id: string,
+        name: string,
+        short: string
+    }
+
+    private constructor(data: CountryResponse["data"]) {
+        this.data = data
+    }
+
+    static from(item: any): CountryResponse | undefined {
+        if (
+            item.id === undefined ||
+            item.name === undefined || item.short === undefined
+        ) return
+
+        return new CountryResponse(item)
+    }
+
+    applyOn(object: Country): void {
+        // Check if data is meant for object
+        if (object.record.join() == this.data.id) return
+
+        if (this.data.name != object.name) object.name = this.data.name
+        if (this.data.short != object.short) object.short = this.data.short
+    }
+
+    intoObject(): Country | undefined {
+        return new Country(this.data.id, this.data.name, this.data.short)
     }
 }

25784152178c5048f637203748b4344b27a671a4
david <RathmairD180116@sus.htl-grieskirchen.at>
2022-12-14 11:41:05 +0100

    Added data in index.tsx

--- frontend/src/pages/client/edit/index.tsx
+++ frontend/src/pages/client/edit/index.tsx
@@ -1,6 +1,8 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import ClientRegisterEditingForm from "../../../components/ClientRegisterEditingForm";
+import Client from "../../../model/client";
+import Address from "../../../model/address";
 
 const Home: NextPage = () => {
     return (
@@ -10,7 +12,7 @@ const Home: NextPage = () => {
             </Head>
 
             <main className="">
-                <ClientRegisterEditingForm/>
+                <ClientRegisterEditingForm client={new Client("Huber", "Fritz", new Date("01.11.2000"), "huber@gmail.com", "0660/123456", false, "01", new Address("Musterstrasse", "13", "4000", "Alkoven")  )}/>
             </main>
         </div>
     )

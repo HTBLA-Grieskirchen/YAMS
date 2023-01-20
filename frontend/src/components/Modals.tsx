ed8dbf24b5cfd321742bdd6fcabb8065e0da220c
TimerErTim <timerertim@gmail.com>
2023-01-20 10:55:06 +0100

    Restyle modals

--- frontend/src/components/Modals.tsx
+++ frontend/src/components/Modals.tsx
@@ -1,15 +1,15 @@
 import {observer} from "mobx-react";
 import {useStore} from "../stores";
 
-const DialogOverlay = observer(() => {
+const Modals = observer(() => {
     const store = useStore()
 
     return <>
         {store.dialogStore.dialogs().map((dialog) =>
             <div key={dialog.uuid} className="flex fixed inset-0 bg-gray-600 bg-opacity-50
-                overflow-y-auto h-full w-full items-center"
-            >
-                <div className="p-2 rounded-lg bg-white self-center h-fit w-fit mx-auto shadow-lg">
+                overflow-y-auto h-full w-full items-center">
+                <div
+                    className={`modal modal-open ${dialog.type === "responsive" ? "modal-bottom md:modal-middle" : ""}`}>
                     {dialog.component(store.dialogStore.closeDialog)}
                 </div>
             </div>
@@ -17,4 +17,4 @@ const DialogOverlay = observer(() => {
     </>
 })
 
-export default DialogOverlay
+export default Modals

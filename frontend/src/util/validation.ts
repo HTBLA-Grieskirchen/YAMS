80d1c0000e0e95d61166ae138dfcd3c45dc2f192
TimerErTim <timerertim@gmail.com>
2023-02-04 10:58:32 +0100

    Add mobile number validation method

--- frontend/src/util/validation.ts
+++ frontend/src/util/validation.ts
@@ -1,9 +1,17 @@
 const RFC2822EmailRegEx =
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
+const PhoneNumberRegEx =
+    /[+|0]([0-9\-()\/.]\s?){6,15}[0-9]$/
+
 
 export function isValidEmail(email: string) {
     return String(email)
         .toLowerCase()
         .match(RFC2822EmailRegEx)
+}
+
+export function isValidMobilenumber(number: string) {
+    return String(number)
+        .match(PhoneNumberRegEx)
 }

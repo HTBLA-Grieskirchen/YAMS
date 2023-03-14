155082ad7c12ef856c53839305e45b67b4bdcc67
TimerErTim <timerertim@gmail.com>
2023-03-14 09:43:44 +0100

    Improve dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
+module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
@@ -47,4 +46,4 @@ module.exports = withMT({
             },
         ],
     },
-})
+}
